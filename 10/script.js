8f6b779f21ec49d67bdda5d9f583407de06b5088
Anastasia Kozaeva <sacret190990@gmail.com>
2016-12-21 23:27:34 +0300

    Add 10th task: Hold Shift to Check Multiple Checkboxes

---
+++ 10/script.js
@@ -0,0 +1,20 @@
+const checkboxes = document.querySelectorAll('.inbox__checkbox');
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
+
+let lastChecked;
+function handleCheck(e) {
+  let inBetween = false;
+  if (e.shiftKey && this.checked) {
+    checkboxes.forEach(checkbox => {
+      if (checkbox === this || checkbox === lastChecked) {
+        inBetween = !inBetween;
+      }
+
+      if (inBetween) {
+        checkbox.checked = true;
+      }
+    });
+  }
+
+  lastChecked = this;
+}
