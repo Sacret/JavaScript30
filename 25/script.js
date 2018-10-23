634d979426a5bbee8f50e3b125f897c8a395c6ff
Sacret <sacret@nm.ru>
2018-10-23 23:50:15 +0300

    Add 25th task: Event Capture, Propagation, Bubbling and Once

---
+++ 25/script.js
@@ -0,0 +1,11 @@
+const divs = document.querySelectorAll('.example div');
+
+function logText(e) {
+  console.log(this.classList.value);
+  // e.stopPropagation();
+}
+
+divs.forEach(div => div.addEventListener('click', logText, {
+  capture: false,
+  once: true,
+}));
